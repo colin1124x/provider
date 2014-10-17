58c7730988dc8ef51cbb9c95611c0cce451fef75
Colin <colin1124x@gmail.com>
2014-10-17 15:10:44 +0800

    .

--- tests/ProviderTest.php
+++ tests/ProviderTest.php
@@ -15,4 +15,17 @@ class ProviderTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(isset($app['x']), '檢查服務榜定');
         $this->assertInstanceOf('stdClass', $app['x'], '檢查註冊實體');
     }
+
+    /**
+     * @expectedException badMethodCallException
+     * @expectedExceptionMessageRegExp /^呼叫未定義方法 \[helloWorld\]$/
+     */
+    public function testException()
+    {
+        $app = new Rde\Application();
+
+        $provider = new Fake\MyServiceProvider($app);
+
+        $provider->helloWorld();
+    }
 }
